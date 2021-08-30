9a79b59495d652b1c71d26476b2fd53dbf62594f
ganapathipalani2018 <ganapathiui@gmail.com>
2021-08-30 16:39:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-9gqm98
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9gqm98)
---
+++ index.js
@@ -0,0 +1,32 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var firstInput = document.querySelector("#firstInp");
+var secondInput = document.querySelector("#secondInp");
+var thirdInput = document.querySelector("#thirdInp");
+var checkBtn = document.querySelector("#checkBtn")
+
+var isOutput = document.querySelector("#isOutput");
+var input = document.querySelector("input")
+
+checkBtn.onclick = function(){
+  const a = firstInput.value;
+  const b = secondInput.value;
+  const c = thirdInput.value;
+  if(a>b && a>c){
+    isOutput.innerHTML = `${'first input value is high: ' + a}`;
+  }
+  else if(b > a && b > c){
+    isOutput.innerHTML = `${'second input value is high: '+ b}`;
+  }
+  else if(c > a && c > b){
+    isOutput.innerHTML = `${'third input value is high: '+ c}`;
+  }
+  else{
+    isOutput.innerHTML = `${'three values are equal: ' + a}`;
+  }
+}
